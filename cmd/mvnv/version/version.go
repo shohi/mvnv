44784bff8408d51125137a399547ca7de31f9474
shohi <oshohi@gmail.com>
2022-06-04 22:00:27 +0800

    Bump version

--- cmd/mvnv/version/version.go
+++ cmd/mvnv/version/version.go
@@ -3,16 +3,18 @@ package version
 import (
 	"encoding/json"
 	"fmt"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
 
-var version = "0.0.1"
+var version = "0.0.2"
 var gitCommit string
 
 type Info struct {
 	Version   string `json:"version"`
 	GitCommit string `json:"gitCommit"`
+	GoVersion string `json:"goVersion"`
 }
 
 // New creates a new `version` subcommand.
@@ -21,9 +23,15 @@ func New() *cobra.Command {
 		Use:   "version",
 		Short: "Print version",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			build, ok := debug.ReadBuildInfo()
+			if !ok {
+				build = &debug.BuildInfo{}
+			}
+
 			info := Info{
 				Version:   version,
 				GitCommit: gitCommit,
+				GoVersion: build.GoVersion,
 			}
 
 			content, err := json.Marshal(info)
